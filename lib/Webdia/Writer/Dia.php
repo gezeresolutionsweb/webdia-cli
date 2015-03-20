916646c35c5cc0b5392bf57c1761acb171310e2a
Sylvain Lévesque <slevesque@gezere.com>
2015-03-20 15:09:14 -0400

    * Modify DIA writer to use Database stencil in replacement of UML stencil. * Protect against invalid html entities in table comments and field comments.

--- lib/Webdia/Writer/Dia.php
+++ lib/Webdia/Writer/Dia.php
@@ -4,68 +4,76 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
     protected function getHeader() {
         $header = '<?xml version="1.0" encoding="UTF-8"?>
             <dia:diagram xmlns:dia="http://www.lysator.liu.se/~alla/dia/">
-            <dia:diagramdata>
-            <dia:attribute name="background">
-            <dia:color val="#ffffff"/>
-            </dia:attribute>
-            <dia:attribute name="paper">
-            <dia:composite type="paper">
-            <dia:attribute name="name">
-            <dia:string>#Letter#</dia:string>
-            </dia:attribute>
-            <dia:attribute name="tmargin">
-            <dia:real val="2.8222"/>
-            </dia:attribute>
-            <dia:attribute name="bmargin">
-            <dia:real val="2.8222"/>
-            </dia:attribute>
-            <dia:attribute name="lmargin">
-            <dia:real val="2.8222"/>
-            </dia:attribute>
-            <dia:attribute name="rmargin">
-            <dia:real val="2.8222"/>
-            </dia:attribute>
-            <dia:attribute name="is_portrait">
-            <dia:boolean val="false"/>
-            </dia:attribute>
-            <dia:attribute name="scaling">
-            <dia:real val="1"/>
-            </dia:attribute>
-            <dia:attribute name="fitto">
-            <dia:boolean val="false"/>
-            </dia:attribute>
-            <dia:attribute name="fitwidth">
-            <dia:int val="1"/>
-            </dia:attribute>
-            <dia:attribute name="fitheight">
-            <dia:int val="1"/>
-            </dia:attribute>
-            </dia:composite>
-            </dia:attribute>
-            <dia:attribute name="grid">
-            <dia:composite type="grid">
-            <dia:attribute name="width_x">
-            <dia:real val="1"/>
-            </dia:attribute>
-            <dia:attribute name="width_y">
-            <dia:real val="1"/>
-            </dia:attribute>
-            <dia:attribute name="visible_x">
-            <dia:int val="1"/>
-            </dia:attribute>
-            <dia:attribute name="visible_y">
-            <dia:int val="1"/>
-            </dia:attribute>
-            </dia:composite>
-            </dia:attribute>
-            <dia:attribute name="guides">
-            <dia:composite type="guides">
-            <dia:attribute name="hguides"/>
-            <dia:attribute name="vguides"/>
-            </dia:composite>
-            </dia:attribute>
+                <dia:diagramdata>
+                    <dia:attribute name="background">
+                        <dia:color val="#ffffff"/>
+                    </dia:attribute>
+                    <dia:attribute name="pagebreak">
+                        <dia:color val="#000099"/>
+                    </dia:attribute>
+                    <dia:attribute name="paper">
+                        <dia:composite type="paper">
+                            <dia:attribute name="name">
+                                <dia:string>#Letter#</dia:string>
+                            </dia:attribute>
+                            <dia:attribute name="tmargin">
+                                <dia:real val="2.5399999618530273"/>
+                            </dia:attribute>
+                            <dia:attribute name="bmargin">
+                                <dia:real val="2.5399999618530273"/>
+                            </dia:attribute>
+                            <dia:attribute name="lmargin">
+                                <dia:real val="2.5399999618530273"/>
+                            </dia:attribute>
+                            <dia:attribute name="rmargin">
+                                <dia:real val="2.5399999618530273"/>
+                            </dia:attribute>
+                            <dia:attribute name="is_portrait">
+                                <dia:boolean val="false"/>
+                            </dia:attribute>
+                            <dia:attribute name="scaling">
+                                <dia:real val="0.62845361232757568"/>
+                            </dia:attribute>
+                            <dia:attribute name="fitto">
+                                <dia:boolean val="true"/>
+                            </dia:attribute>
+                            <dia:attribute name="fitwidth">
+                                <dia:int val="1"/>
+                            </dia:attribute>
+                            <dia:attribute name="fitheight">
+                                <dia:int val="1"/>
+                            </dia:attribute>
+                        </dia:composite>
+                </dia:attribute>
+                <dia:attribute name="grid">
+                    <dia:composite type="grid">
+                        <dia:attribute name="width_x">
+                            <dia:real val="1"/>
+                        </dia:attribute>
+                        <dia:attribute name="width_y">
+                            <dia:real val="1"/>
+                        </dia:attribute>
+                        <dia:attribute name="visible_x">
+                            <dia:int val="1"/>
+                        </dia:attribute>
+                        <dia:attribute name="visible_y">
+                            <dia:int val="1"/>
+                        </dia:attribute>
+                        <dia:composite type="color"/>
+                    </dia:composite>
+                </dia:attribute>
+                <dia:attribute name="color">
+                    <dia:color val="#d8e5e5"/>
+                </dia:attribute>
+                <dia:attribute name="guides">
+                    <dia:composite type="guides">
+                        <dia:attribute name="hguides"/>
+                        <dia:attribute name="vguides"/>
+                    </dia:composite>
+                </dia:attribute>
             </dia:diagramdata>
-            <dia:layer name="Segundo Plano" visible="true">';
+            <dia:layer name="Arrière-plan" visible="true" active="true">
+            ';
 
         return $header;
     }
@@ -77,13 +85,16 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
         return $footer;
     }
 
-    protected function getObjectHeader( $x1, $y1, $x2, $y2, $x3, $y3, $width, $height, $name ) {
-        $objectHeader = '<dia:object type="UML - Class" version="0" id="O0">
+    protected function getObjectHeader( $x1, $y1, $x2, $y2, $x3, $y3, $width, $height, $name, $comment ) {
+        $objectHeader = '<dia:object type="Database - Table" version="0" id="o0' . uniqid() . '">
             <dia:attribute name="obj_pos">
             <dia:point val="' . $x1 . ',' . $y1 . '"/>
             </dia:attribute>
             <dia:attribute name="obj_bb">
             <dia:rectangle val="' . $x2 . ',' . $y2 . ';' . $x3 . ',' . $y3 . '"/>
+            </dia:attribute>
+            <dia:attribute name="meta">
+            <dia:composite type="dict"/>
             </dia:attribute>
             <dia:attribute name="elem_corner">
             <dia:point val="' . $x1 . ',' . $y1 . '"/>
@@ -94,56 +105,53 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
             <dia:attribute name="elem_height">
             <dia:real val="' . $height . '"/>
             </dia:attribute>
+            <dia:attribute name="text_colour">
+            <dia:color val="#000000"/>
+            </dia:attribute>
+            <dia:attribute name="line_colour">
+            <dia:color val="#000000"/>
+            </dia:attribute>
+            <dia:attribute name="fill_colour">
+            <dia:color val="#ffffff"/>
+            </dia:attribute>
+            <dia:attribute name="line_width">
+            <dia:real val="0.10000000000000001"/>
+            </dia:attribute>
             <dia:attribute name="name">
             <dia:string>#' . $name . '#</dia:string>
             </dia:attribute>
-            <dia:attribute name="stereotype">
-            <dia:string/>
+            <dia:attribute name="comment">
+            <dia:string>#' . htmlspecialchars( $comment ) . '#</dia:string>
             </dia:attribute>
-            <dia:attribute name="abstract">
+            <dia:attribute name="visible_comment">
             <dia:boolean val="false"/>
             </dia:attribute>
-            <dia:attribute name="suppress_attributes">
+            <dia:attribute name="tagging_comment">
             <dia:boolean val="false"/>
             </dia:attribute>
-            <dia:attribute name="suppress_operations">
-            <dia:boolean val="false"/>
-            </dia:attribute>
-            <dia:attribute name="visible_attributes">
+            <dia:attribute name="underline_primary_key">
             <dia:boolean val="true"/>
             </dia:attribute>
-            <dia:attribute name="visible_operations">
-            <dia:boolean val="false"/>
-            </dia:attribute>
-            <dia:attribute name="foreground_color">
-            <dia:color val="#000000"/>
-            </dia:attribute>
-            <dia:attribute name="background_color">
-            <dia:color val="#ffffff"/>
+            <dia:attribute name="bold_primary_keys">
+            <dia:boolean val="true"/>
             </dia:attribute>
             <dia:attribute name="normal_font">
-            <dia:font name="Courier"/>
+            <dia:font family="monospace" style="0" name="courier"/>
             </dia:attribute>
-            <dia:attribute name="abstract_font">
-            <dia:font name="Courier-Oblique"/>
+            <dia:attribute name="name_font">
+            <dia:font family="sans" style="80" name="helvetica-bold"/>
             </dia:attribute>
-            <dia:attribute name="classname_font">
-            <dia:font name="Helvetica-Bold"/>
+            <dia:attribute name="comment_font">
+            <dia:font family="sans" style="8" name="helvetica-oblique"/>
             </dia:attribute>
-            <dia:attribute name="abstract_classname_font">
-            <dia:font name="Helvetica-BoldOblique"/>
+            <dia:attribute name="normal_font_height">
+            <dia:real val="0.80000000000000004"/>
             </dia:attribute>
-            <dia:attribute name="font_height">
-            <dia:real val="0.8"/>
+            <dia:attribute name="name_font_height">
+            <dia:real val="0.69999999999999996"/>
             </dia:attribute>
-            <dia:attribute name="abstract_font_height">
-            <dia:real val="0.8"/>
-            </dia:attribute>
-            <dia:attribute name="classname_font_height">
-            <dia:real val="1"/>
-            </dia:attribute>
-            <dia:attribute name="abstract_classname_font_height">
-            <dia:real val="1"/>
+            <dia:attribute name="comment_font_height">
+            <dia:real val="0.69999999999999996"/>
             </dia:attribute>
             <dia:attribute name="attributes">';
 
@@ -152,35 +160,113 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
 
     protected function getObjectFooter() {
         $objectFooter = '</dia:attribute>
-            <dia:attribute name="operations"/>
-            <dia:attribute name="template">
-            <dia:boolean val="false"/>
-            </dia:attribute>
-            <dia:attribute name="templates"/>
             </dia:object>';
 
         return $objectFooter;
     }
 
-    protected function getAttribute( $name, $type, $default ) {
-        $attribute = '<dia:composite type="umlattribute">
+
+    /* TO BE INTEGRATE LATER.
+    public function getConnectionHeader() {
+        return '<dia:connections>';
+    }
+    public function getConnectionFooter() {
+        return '</dia:connections>';
+    }
+    public function addConnection() {
+      $connection = '<dia:connection handle="0" to="O0" connection="15"/>';
+    }
+     */
+    public function getDatabaseReference() {
+
+        $databaseReference = '<dia:object type="Database - Reference" version="0" id="O2">
+            <dia:attribute name="obj_pos">
+            <dia:point val="20.96,16.25"/>
+            </dia:attribute>
+            <dia:attribute name="obj_bb">
+            <dia:rectangle val="20.96,14.9;33.5,16.25"/>
+            </dia:attribute>
+            <dia:attribute name="meta">
+            <dia:composite type="dict"/>
+            </dia:attribute>
+            <dia:attribute name="orth_points">
+            <dia:point val="20.96,16.25"/>
+            <dia:point val="27.23,16.25"/>
+            <dia:point val="27.23,15.55"/>
+            <dia:point val="33.5,15.55"/>
+            </dia:attribute>
+            <dia:attribute name="orth_orient">
+            <dia:enum val="0"/>
+            <dia:enum val="1"/>
+            <dia:enum val="0"/>
+            </dia:attribute>
+            <dia:attribute name="orth_autoroute">
+            <dia:boolean val="false"/>
+            </dia:attribute>
+            <dia:attribute name="text_colour">
+            <dia:color val="#000000"/>
+            </dia:attribute>
+            <dia:attribute name="line_colour">
+            <dia:color val="#000000"/>
+            </dia:attribute>
+            <dia:attribute name="line_width">
+            <dia:real val="0.10000000000000001"/>
+            </dia:attribute>
+            <dia:attribute name="line_style">
+            <dia:enum val="0"/>
+            <dia:real val="1"/>
+            </dia:attribute>
+            <dia:attribute name="corner_radius">
+            <dia:real val="0"/>
+            </dia:attribute>
+            <dia:attribute name="end_arrow">
+            <dia:enum val="0"/>
+            </dia:attribute>
+            <dia:attribute name="start_point_desc">
+            <dia:string>#0..n#</dia:string>
+            </dia:attribute>
+            <dia:attribute name="end_point_desc">
+            <dia:string>#1#</dia:string>
+            </dia:attribute>
+            <dia:attribute name="normal_font">
+            <dia:font family="monospace" style="0" name="Courier"/>
+            </dia:attribute>
+            <dia:attribute name="normal_font_height">
+            <dia:real val="0.59999999999999998"/>
+            </dia:attribute>
+            <dia:connections>
+            <dia:connection handle="0" to="O0" connection="15"/>
+            <dia:connection handle="1" to="O1" connection="12"/>
+            </dia:connections>
+            </dia:object>';
+        return $databaseReference;
+    }
+
+
+
+    protected function getAttribute( $field ) {
+        $primaryKey = ( (int) $field[ 'primary_key' ] === 1 ) ? 'true' : 'false';
+        $nullable = ( $field[ 'nullable' ] === 'NO' ) ? 'false' : 'true';
+        $unique = ( (int) $field[ 'unique' ] === 1 ) ? 'true' : 'false';
+
+        $attribute = '<dia:composite type="table_attribute">
             <dia:attribute name="name">
-            <dia:string>#' . $name . '#</dia:string>
+            <dia:string>#' . $field[ 'name' ] . '#</dia:string>
             </dia:attribute>
             <dia:attribute name="type">
-            <dia:string>#' . $type . '#</dia:string>
+            <dia:string>#' . $field[ 'type' ] . '#</dia:string>
             </dia:attribute>
-            <dia:attribute name="value">
-            <dia:string>#' . $default . '#</dia:string>
+            <dia:attribute name="comment">
+            <dia:string>#' . htmlspecialchars( $field[ 'comment' ] ) . '#</dia:string>
             </dia:attribute>
-            <dia:attribute name="visibility">
-            <dia:enum val="1"/>
+            <dia:attribute name="primary_key">
+            <dia:boolean val="' . $primaryKey . '"/>
             </dia:attribute>
-            <dia:attribute name="abstract">
-            <dia:boolean val="false"/>
+            <dia:attribute name="nullable">
+            <dia:boolean val="' . $nullable . '"/>
             </dia:attribute>
-            <dia:attribute name="class_scope">
-            <dia:boolean val="false"/>
+            <dia:attribute name="unique">
+            <dia:boolean val="' . $unique . '"/>
             </dia:attribute>
             </dia:composite>';
 
@@ -217,14 +303,16 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
                 $y3 += $yinc;
             }
 
-            $xml .= $this->getObjectHeader( $x1, $y1, $x2, $y2, $x3, $y3, $width, $height, $table[ 'name' ] );
+            $xml .= $this->getObjectHeader( $x1, $y1, $x2, $y2, $x3, $y3, $width, $height, $table[ 'name' ], $table[ 'comment' ] );
 
             foreach( $this->reader->getFields( $table[ 'name' ] ) as $field ) {
-                $xml .= $this->getAttribute( $field[ 'name' ], $field[ 'type' ], $field[ 'default' ] );
+                $xml .= $this->getAttribute( $field );
             }
 
             $xml .= $this->getObjectFooter();
         }
+
+        $xml .= $this->getDatabaseReference();
 
         $xml .= $this->getFooter();
 
@@ -244,3 +332,4 @@ class Webdia_Writer_Dia extends Webdia_Writer implements Webdia_Writer_Interface
         fwrite( $fp, $xml );
     }
 }
+
